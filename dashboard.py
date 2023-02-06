11506b2abbb30b55fcd1acdab8a87012fd7bcfbc
varun-cloudknox <vkolli@cloudknox.io>
2023-02-06 12:38:10 -0800

    tick

--- dashboard.py
+++ dashboard.py
@@ -24,16 +24,12 @@ warnings.filterwarnings('ignore')
 
 def getAge():
     options = ['18-20', '21-24', '25-29', '30-34', '35-39', '40-44', '45-49', '50-54', '55-59', '60-64', '65 up']
-
-    def custom_slider(options):
-        value = st.slider("Select Age Range:", min_value=0, max_value=len(options)-1, value=0, step=1, format=None)
-        st.write("\n".join([f"{i}: {options[i]}" for i in range(len(options))]), format=None)
-        return value
-
-    selected_option = custom_slider(options)
+    ticks = list(range(len(options)))
+    selected_option = st.slider("Select Age Range:", min_value=0, max_value=len(options)-1, value=0, step=1, format=None, key=None, handle_color=None, disabled=False, dynamic_slider_value=False, slider_color=None, label_width=None, min_step=None, max_step=None, interval=None, get_interval=None, marks=dict(zip(ticks, options)), class_name=None, persistence=True, persistence_type='local', reset_button=False)
     selected_age_range = options[selected_option]
 
     st.write(f"You selected: {selected_age_range}")
+
 
 def main():
     st.title("Predicting Mental Health Disorder from Demographic Information")
