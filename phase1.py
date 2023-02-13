a6063e8a43a7be779ecb6d3753f3b9d7897889b6
varun-cloudknox <vkolli@cloudknox.io>
2023-02-12 18:22:38 -0800

    smt

---
+++ phase1.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+"""
+about the dataset
+- cleaning the dataset
+- handling null
+- convert to categorical
+
+three different models run
+- score
+"""
+
+
+st.title("Making the Model")
+header = st.sidebar.header("About the Data")
+
+data_info = "blah blah blah"
+if header:
+    st.markdown(data_info)
+
+st.write("Welcome to my app!")
