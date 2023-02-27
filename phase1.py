62270ff040f374e506df4624031484b06c5e7181
varun-cloudknox <vkolli@cloudknox.io>
2023-02-26 20:13:58 -0800

    update

--- phase1.py
+++ phase1.py
@@ -33,7 +33,7 @@ def introduction():
 def v1():
     st.title('Version 1: Decision Tree Model Hyperparameter Tuning Report')
     data = {
-       'Hyperparameters': [Grid Search 1', 'Grid Search 2'],
+       'Hyperparameters': ['Grid Search 1', 'Grid Search 2'],
        'F1-Score Weighted': [0.493, 0.493],
        'Max Depth': [60, 45],
        'Min Samples Leaf': [45, 56],
