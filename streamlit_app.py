12b594880ff5f81f676c17aff607bd6915f944aa
varun-cloudknox <vkolli@cloudknox.io>
2023-03-12 16:14:59 -0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 
 # Read in the CSV file
-df = pd.read_csv("data.csv")
+df = pd.read_csv("CSV_files/data.csv")
 
 # Write out the first 10 rows of the DataFrame
 st.write("First 10 rows:", df.head(10))
