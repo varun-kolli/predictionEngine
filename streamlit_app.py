c84045edcb62b72a2bbe403d9b93015bf3d1f74f
varun-cloudknox <vkolli@cloudknox.io>
2023-03-12 17:08:11 -0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,20 @@
 import pandas as pd
 import streamlit as st
-from phase1 import *
+from phase1 import introduction, v1, v2, v3, v4
 from sidebar import sideBar
 
 def initializeSideBar():
-    while True:
-        selection = sideBar()
-        if selection[0]:
-            introduction()
-        if selection[1]:
-            v1()
-        if selection[2]:
-            v2()
-        if selection[3]:
-            v3()
-        if selection[4]:
-            v4()
+    selection = sideBar()
+    if selection[0]:
+        introduction()
+    if selection[1]:
+        v1()
+    if selection[2]:
+        v2()
+    if selection[3]:
+        v3()
+    if selection[4]:
+        v4()
 
 def main():
     initializeSideBar()
