717c673b1d77197ca91e3e0ddf5c46e0a5620729
varun-cloudknox <vkolli@cloudknox.io>
2023-03-12 22:14:04 -0700

    update

--- dataCleaning.py
+++ dataCleaning.py
@@ -11,7 +11,7 @@ def v2():
     # loop over all columns in the dataframe and get their unique values
     for column in smt.columns:
         unique_values = smt[column].unique()
-        print(f"Unique values in column '{column}': {unique_values}")
+        st.write(f"Unique values in column '{column}': {unique_values}")
 
 
     st.subheader("1. Filtering")
