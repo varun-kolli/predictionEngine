bf6ef73c5ca80e3cb9107e931d479d9dace0a77f
varun-cloudknox <vkolli@cloudknox.io>
2023-03-12 20:04:34 -0700

    update

--- handlingNull.py
+++ handlingNull.py
@@ -4,32 +4,29 @@ from PIL import Image
 
 
 def t1():
-    st.markdown(":red[F1 score: 0.469]")
     image = Image.open('images/nullsRemoved.jpg')
     st.image(image, caption='Decision Matrix and Feature Importance')
 
 def t2():
     st.write("")
-    st.markdown(":red[F1 score: 0.502]")
     image = Image.open('images/nullsDistribution.jpg')
-    st.image(image, caption='Decision Matrix and Feature Importance')
 
 def t3():
     st.write("")
-    st.markdown(":green[F1 score: 0.503]")
     image = Image.open('images/nullsMode.jpg')
-    st.image(image, caption='Decision Matrix and Feature Importance')
 
 def v3():
     st.header("Handling Missing Values")
     st.write("")
     st.write("Current F1 Score: **0.493**")
     st.write("To improve our F1 score, we decided to train 3 models that handle null values in different ways:")
-    st.write("a. Removing Null Values")
-    st.write("b. Replacing with Distribution Values")
-    st.write("c. Replacing with Mode Values")
+    data = {'methods': ['Removing Null Values', 'Replacing with Distribution Values', 'Replacing with Mode Values'],
+            'f1 score': [0.469, 0.502, 0.503]}
 
+    df = pd.DataFrame(data)
+    st.write(df)
 
+    st.subheader("Decision Matrix and Classification Reports")
 
     tab1, tab2, tab3 = st.tabs(["Removing", "Distribution", "Mode"])
     with tab1:
