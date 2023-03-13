c25c4317b5939164098fdb043064096ffa68419b
varun-cloudknox <vkolli@cloudknox.io>
2023-03-13 16:53:24 -0700

    update

--- backendInterface.py
+++ backendInterface.py
@@ -77,6 +77,7 @@ def testRun(input):
     print(df_dummies.columns)
     queryRow = np.array(df_dummies.iloc[0]).reshape(1, -1)
     prediction = model.predict(queryRow)
+    st.write(prediction)
 
 def display_user_input(user_input):
         variable_names = ['Age', 'Education', 'Ethnicity', 'Race', 'Gender', 'Marital Status', 'SAP', 'Employment Status', 'Living Arrangement', 'Veteran Status', 'State']
