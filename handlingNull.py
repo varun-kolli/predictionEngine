d3c04966e722dedb0ba27fb7b06649f85d418911
varun-cloudknox <vkolli@cloudknox.io>
2023-03-12 19:56:01 -0700

    update

--- handlingNull.py
+++ handlingNull.py
@@ -2,25 +2,37 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 
-def v3():
-    st.header("Handling Missing Values")
-    st.write("")
-    st.write("Current F1 Score: **0.493**")
-    st.write("To improve our F1 score, we decided to train 3 models that handle null values in different ways:")
 
+def t1():
     st.write("a. Removing Null Values")
     st.markdown(":red[F1 score: 0.469]")
     image = Image.open('images/nullsRemoved.jpg')
     st.image(image, caption='Decision Matrix and Feature Importance')
 
+def t2():
     st.write("")
     st.write("b. Replacing with Distribution Values")
     st.markdown(":red[F1 score: 0.502]")
     image = Image.open('images/nullsDistribution.jpg')
     st.image(image, caption='Decision Matrix and Feature Importance')
 
+def t3():
     st.write("")
     st.write("c. Replacing with Mode Values")
     st.markdown(":green[F1 score: 0.503]")
     image = Image.open('images/nullsMode.jpg')
     st.image(image, caption='Decision Matrix and Feature Importance')
+
+def v3():
+    st.header("Handling Missing Values")
+    st.write("")
+    st.write("Current F1 Score: **0.493**")
+    st.write("To improve our F1 score, we decided to train 3 models that handle null values in different ways:")
+
+    tab1, tab2, tab3 = st.tabs(["Removing", "Distribution", "Mode"])
+    with tab1:
+        t1()
+    with tab2:
+        t2()
+    with tab3:
+        t3()
