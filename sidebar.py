f594a0688d2c469129ab0202603aa8daf7e9842b
varun-cloudknox <vkolli@cloudknox.io>
2023-03-12 17:13:34 -0700

    update

--- sidebar.py
+++ sidebar.py
@@ -6,26 +6,38 @@ def sideBar():
 
     # Create a sidebar with buttons
     m = st.markdown("""
-    <style>
-    div.stButton > button:first-child {
-        background-color: #007bff;
-        color: #ffffff;
-    }
-    div.stButton > button:hover {
-        background-color: #0069d9;
-        color: #ffffff;
-    }
-    div.stButton > button:focus {
-        background-color: #0056b3;
-        color: #ffffff;
-    }
-    </style>""", unsafe_allow_html=True)
+        <style>
+        /* Style for selected tabs */
+        .streamlit-tabs .streamlit-tab[data-baseweb="tab"] > button[data-baseweb="button"][aria-selected="true"] {
+            background-color: #007bff;
+            color: #ffffff;
+        }
+        .streamlit-tabs .streamlit-tab[data-baseweb="tab"] > button[data-baseweb="button"][aria-selected="true"]:hover {
+            background-color: #0069d9;
+            color: #ffffff;
+        }
+        .streamlit-tabs .streamlit-tab[data-baseweb="tab"] > button[data-baseweb="button"][aria-selected="true"]:focus {
+            background-color: #0056b3;
+            color: #ffffff;
+        }
+
+        /* Style for unselected tabs */
+        .streamlit-tabs .streamlit-tab[data-baseweb="tab"] > button[data-baseweb="button"]:not([aria-selected="true"]):hover {
+            background-color: #f0f0f0;
+            color: #007bff;
+        }
+        .streamlit-tabs .streamlit-tab[data-baseweb="tab"] > button[data-baseweb="button"]:not([aria-selected="true"]):focus {
+            background-color: #e5e5e5;
+            color: #007bff;
+        }
+        </style>
+    """, unsafe_allow_html=True)
 
     tabs = ["Introduction", "Initial Modelling", "Decision Tree Improvements", "Data Balancing", "Random Forest"]
 
     # Create a sidebar with buttons for each tab
     st.sidebar.markdown("Navigation")
-    selection = [st.sidebar.button(tab, key=tab, help=tab, type='primary') for tab in tabs]
+    selection = [st.sidebar.button(tab, key=tab, help=tab, type='secondary') for tab in tabs]
     return selection
 
 """
