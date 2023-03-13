297c69ffdf905cf14773b55a4dbc0f45c35170b8
varun-cloudknox <vkolli@cloudknox.io>
2023-03-12 20:06:39 -0700

    update

--- handlingNull.py
+++ handlingNull.py
@@ -5,15 +5,16 @@ from PIL import Image
 
 def t1():
     image = Image.open('images/nullsRemoved.jpg')
-    st.image(image, caption='Decision Matrix and Feature Importance')
+    st.image(image)
 
 def t2():
-    st.write("")
     image = Image.open('images/nullsDistribution.jpg')
+    st.image(image)
 
 def t3():
-    st.write("")
     image = Image.open('images/nullsMode.jpg')
+    st.image(image)
+
 
 def v3():
     st.header("Handling Missing Values")
@@ -21,12 +22,12 @@ def v3():
     st.write("Current F1 Score: **0.493**")
     st.write("To improve our F1 score, we decided to train 3 models that handle null values in different ways:")
     data = {'methods': ['Removing Null Values', 'Replacing with Distribution Values', 'Replacing with Mode Values'],
-            'f1 score': [0.469, 0.502, 0.503]}
+            'F1 score': [0.469, 0.502, 0.503]}
 
     df = pd.DataFrame(data)
     st.write(df)
 
-    st.subheader("Decision Matrix and Classification Reports")
+    st.subheader("Decision Matrix and Feature Importance")
 
     tab1, tab2, tab3 = st.tabs(["Removing", "Distribution", "Mode"])
     with tab1:
