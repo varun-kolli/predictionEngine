87628494f9b847d9e62358f0ae747288d7417adb
varun-cloudknox <vkolli@cloudknox.io>
2023-03-12 21:23:16 -0700

    update

--- distributionPkl.py
+++ distributionPkl.py
@@ -3,12 +3,25 @@ import streamlit as st
 import joblib
 import numpy as np
 
-def dist():
-    st.write("Distribution Null Model")
-    model = joblib.load('pkl_files/dt_dist.sav')
-
+@st.cache_data
+def loadFile():
     fileName = "CSV_files/data.csv"
     df = pd.read_csv(fileName).dropna()
     df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+    return df
+
+@st.cache_data
+def loadModel():
+    model = joblib.load('pkl_files/dt_dist.sav')
+    return model
+
+def dist():
+    st.write("Distribution Null Model")
+    model = loadModel()
+    file = loadFile()
+
+
+
+    return df
 
     st.write(df)
