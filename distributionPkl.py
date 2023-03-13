e362fa457745d57fb864ddf1109ff4d26423d36f
varun-cloudknox <vkolli@cloudknox.io>
2023-03-12 21:00:10 -0700

    update

--- distributionPkl.py
+++ distributionPkl.py
@@ -5,14 +5,10 @@ import numpy as np
 
 def dist():
     st.write("Distribution Null Model")
-    # Load the model from the .sav file
     model = joblib.load('pkl_files/dt_dist.sav')
 
-    # Prepare the input data for the model
-    input_data = np.array([[1, 2, 3, 4]])  # replace with your own input data
+    fileName = "data.csv"
+    df = pd.read_csv(fileName).dropna()
+    df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
 
-    # Use the loaded model to make predictions
-    output = model.predict(input_data)
-
-    # Print the output
-    print(output)
+    st.write(df)
