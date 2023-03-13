767895db42c3e0b383e1ee05c809aee0aaec7592
varun-cloudknox <vkolli@cloudknox.io>
2023-03-13 16:50:27 -0700

    update

--- backendInterface.py
+++ backendInterface.py
@@ -35,15 +35,49 @@ def convertAge(age):
 
     return age_group_str
 
+@st.cache_data
+def getModel(df):
+    input = ['2442501', "21-24", "Bipolar" , "12+", "Other Hispanic or Latino origin", "White","Male", "Never married", "Yes", "Part time", "Other", "2", "TN"]
+    df.iloc[0] = input
+    x = pd.get_dummies(df.drop(columns = ['MH1', 'Unnamed: 0']), drop_first = True)
+    y = df['MH1']
+    train_x, test_x, train_y, test_y = train_test_split(x, y, test_size = 0.4, random_state = 1)
+    folds = KFold(n_splits = 5, shuffle = True, random_state = 1)
+    clf = DecisionTreeClassifier(random_state = 42)
+
+    hyper_params_new = {
+        'max_depth': list(range(50, 55)),
+        'min_samples_split': list(range(1, 7)),
+        'min_samples_leaf': list(range(42, 47))
+    }
+
+    # Call GridSearchCV()
+    model_clf = GridSearchCV(estimator = clf,
+                            param_grid = hyper_params_new,
+                            scoring = 'f1_weighted', # Use a suitable regression metric
+                            cv = folds,
+                            verbose = 1,
+                            n_jobs = -1) # Will utilize all available CPUs
+
+    model_clf.fit(train_x, train_y)
+    input = np.array(test_x.iloc[10]).reshape(1, -1)
+
+    return model_clf
+
+def testRun(user_input):
+    fileName = "data.csv"
+    df = pd.read_csv(fileName).dropna()
+    df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+    df_copy = df.copy()
+    model = getModel(df_copy)
+
 def display_user_input(user_input):
         variable_names = ['Age', 'Education', 'Ethnicity', 'Race', 'Gender', 'Marital Status', 'SAP', 'Employment Status', 'Living Arrangement', 'Veteran Status', 'State']
         df = pd.DataFrame({'Variable': variable_names, 'Value': user_input})
         st.table(df)
         user_input.insert(0, "1207979")
         user_input.insert(2, "Trauma-related")
-        st.write(user_input)
-        input = ['2442501', "21-24", "Bipolar" , "12+", "Other Hispanic or Latino origin", "White","Male", "Never married", "Yes", "Part time", "Other", "2", "TN"]
-        st.write(input)
+        testRun(user_input)
 
 def prompt():
     if "user" not in st.session_state:
