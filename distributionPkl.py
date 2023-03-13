70916f87ee6bc0d10de6b30e45430d9eca4488ae
varun-cloudknox <vkolli@cloudknox.io>
2023-03-12 20:50:12 -0700

    update

--- distributionPkl.py
+++ distributionPkl.py
@@ -6,7 +6,7 @@ import numpy as np
 def dist():
     st.write("Distribution Null Model")
     # Load the model from the .sav file
-    model = joblib.load('model.sav')
+    model = joblib.load('pkl_files/dt_dist.sav')
 
     # Prepare the input data for the model
     input_data = np.array([[1, 2, 3, 4]])  # replace with your own input data
