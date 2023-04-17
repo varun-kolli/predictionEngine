1b12cf0e8dc03654bc0142b6a77ae6eaf641e5c7
varun-cloudknox <vkolli@cloudknox.io>
2023-04-16 21:50:29 -0700

    update

--- correlation.py
+++ correlation.py
@@ -4,3 +4,4 @@ from PIL import Image
 
 def correlation_main():
     st.title("Correlation")
+    st.write("After attempting a random forest model, found that training it would be computationally expensive with the current data organization. We then decided to train an unsupervised learning models to create clusters of patients with similar disorders to simply the target variable any model will interpret.")
