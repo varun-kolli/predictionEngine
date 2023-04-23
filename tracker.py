4efb8204fe85ce085b80b0d1efaae484ff72602f
varun-cloudknox <vkolli@cloudknox.io>
2023-04-22 20:35:42 -0700

    update

--- tracker.py
+++ tracker.py
@@ -2,4 +2,11 @@ import pandas as pd
 import streamlit as st
 
 def report():
-    st.title("Model Tracker")
+    st.markdown(
+        f"""
+        <div style='text-align:center'>
+            <h1>Model Tracker</h1>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
