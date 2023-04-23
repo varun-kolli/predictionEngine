d94b738a22d42ab3dcf1153d6bda65931d94745b
varun-cloudknox <vkolli@cloudknox.io>
2023-04-22 20:20:07 -0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,11 @@ def main():
         page_icon="🧊",
         layout="wide",
         initial_sidebar_state="expanded",
+        menu_items={
+            'Get Help': 'https://www.extremelycoolapp.com/help',
+            'Report a bug': "https://www.extremelycoolapp.com/bug",
+            'About': "# This is a header. This is an *extremely* cool app!"
+        }
     )
     initializeSideBar()
 
