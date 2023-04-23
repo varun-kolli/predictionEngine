f71098aaf0a2d7f3e658f4a6596a1700d704c6ff
varun-cloudknox <vkolli@cloudknox.io>
2023-04-22 17:15:08 -0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ def initializeSideBar():
     if selection[5]:
         correlation_main()
     if selection[6] or all(value == False for value in selection) :
-        interface()
+        introduction()
 
 
 def main():
