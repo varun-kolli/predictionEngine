61325e8fe10f9d40f09eab3601e50921927867e3
varun-cloudknox <vkolli@cloudknox.io>
2023-04-22 21:39:27 -0700

    update

--- tracker.py
+++ tracker.py
@@ -16,6 +16,16 @@ def report():
     st.write("First Attempt with the partially cleaned SAMDHA data resulted in a low F1 score of <span style='color:red'>0.493</span>.", unsafe_allow_html=True)
     st.caption("5 Fold Cross Validation       Training Size: 70%      Test Size: 30%")
 
-    st.header("Data Cleaning")
+    st.header("Data Processing")
+
+    st.write("Methods:")
+    bullet_points = [
+        "Filtering disorders to **Bipolar, Trauma, Depression**",
+        "3 different approaches to handling null values: Remove Null Values, Replace with Distribution, Replace with Mode",
+        "Converted relevant input column data to categorical variables"
+    ]
+
+    for bullet_point in bullet_points:
+        st.markdown(f"* {bullet_point}")
 
     st.subheader("")
