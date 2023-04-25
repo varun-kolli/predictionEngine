7e887df7f2e096c1c6a5b81f51bd3a4c85a7d3b4
varun-cloudknox <vkolli@cloudknox.io>
2023-04-24 22:52:20 -0700

    update

--- tracker.py
+++ tracker.py
@@ -47,12 +47,12 @@ def report():
     with st.container():
         col1, col2, col3 = st.beta_columns(3)
         with col1:
-                st.metric(label="Metric 1", value=5)
+            st.metric(label="Metric 1", value=5)
 
-            with col2:
-                st.metric(label="Metric 2", value=5)
+        with col2:
+            st.metric(label="Metric 2", value=5)
 
-            with col3:
-                st.metric(label="Metric 3", value=5)
+        with col3:
+            st.metric(label="Metric 3", value=5)
 
 
