2a42fe2ae68bea97fa18d8ac64331e683d0e06fc
varun-cloudknox <vkolli@cloudknox.io>
2023-04-24 23:17:09 -0700

    update

--- tracker.py
+++ tracker.py
@@ -55,7 +55,7 @@ def report():
        with col3:
            st.metric(label="Distribution", value=0.502, delta=round(0.009, 4))
 
-    with open("my_custom_theme.css")as f:
-     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
+    with open("my_custom_theme.css") as f:
+     st.markdown(f"<style>{f.read()}</style>" , unsafe_allow_html = True)
 
 
