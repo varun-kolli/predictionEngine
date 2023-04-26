69e05431e408d9d4c369e0a642aedc5fc0457bb0
varun-cloudknox <vkolli@cloudknox.io>
2023-04-25 20:52:05 -0700

    update

--- sidebar.py
+++ sidebar.py
@@ -6,7 +6,8 @@ def sideBar():
     tabs2 = ["Backend Interface"]
 
     st.sidebar.subheader("Navigation")
-    selection = [st.sidebar.button(tab, key=tab, help=tab, type='secondary') for tab in tabs1]
-    selection += [st.sidebar.button(tab, key=tab, help=tab, type='secondary') for tab in tabs2]
+    with st.container:
+        selection = [st.sidebar.button(tab, key=tab, help=tab, type='secondary') for tab in tabs1]
+        selection += [st.sidebar.button(tab, key=tab, help=tab, type='secondary') for tab in tabs2]
 
     return selection
