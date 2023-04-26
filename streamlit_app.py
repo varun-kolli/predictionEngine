81eea4964f87cda180bfcffc8d7fdb196d0ceca8
varun-cloudknox <vkolli@cloudknox.io>
2023-04-26 15:40:56 -0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ def main():
     initializeSideBar()
 
 if __name__ == '__main__':
-    st.set_page_config(layout='wide')
+
     with open("style.css") as f:
      st.markdown(f"<style>{f.read()}</style>" , unsafe_allow_html = True)
     main()
