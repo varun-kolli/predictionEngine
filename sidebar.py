eb1c152e12c3ec2687f3fa5df56e7d6432a03663
varun-cloudknox <vkolli@cloudknox.io>
2023-04-25 20:46:38 -0700

    update

--- sidebar.py
+++ sidebar.py
@@ -5,7 +5,7 @@ def sideBar():
     tabs1 = ["Introduction", "Initial Modelling", "Data Cleaning", "Handling Null Values", "Data Balancing", "Correlation", "Model Tracker"]
     tabs2 = ["Backend Interface"]
 
-    st.sidebar.markdown("Navigation")
+    st.sidebar.subheader("Navigation")
     selection = [st.sidebar.button(tab, key=tab, help=tab, type='secondary') for tab in tabs1]
     selection += [st.sidebar.button(tab, key=tab, help=tab, type='secondary') for tab in tabs2]
 
