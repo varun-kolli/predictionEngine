0201befda5b6394d7a61920453dc13102cd26bda
varun-cloudknox <vkolli@cloudknox.io>
2023-04-26 19:50:40 -0700

    update

--- sidebar.py
+++ sidebar.py
@@ -2,8 +2,7 @@ import pandas as pd
 import streamlit as st
 
 def sideBar():
-    tabs = ["Introduction", "Initial Modelling", "Data Cleaning", "Handling Null Values", "Data Balancing", "Correlation", "Model Tracker"]
-    tabs.append("Backend Interface")
+    tabs = ["Home", "Predictive Dashboard" "Methodology", "Data Description", "Data Cleaning", "Clustering", "Machine Learning", "Results"]
 
     st.sidebar.subheader("Navigation")
     selection = [st.sidebar.button(tab, key=tab, help=tab, type ="secondary") for tab in tabs]
