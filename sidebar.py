e8c16bd35981a6631aac5c088995c9017b50fb61
varun-cloudknox <vkolli@cloudknox.io>
2023-04-28 14:06:49 -0700

    update

--- sidebar.py
+++ sidebar.py
@@ -2,20 +2,11 @@ import pandas as pd
 import streamlit as st
 
 def sideBar():
+    tabs = ["Home", "Predictive Dashboard", "Methodology", "Results"]
+    methodology_tabs = ["Data Description", "Data Cleaning", "Clustering"]
+    st.sidebar.subheader("Navigation")
 
-    def e1():
-        tabs = ["Home", "Predictive Dashboard", "Methodology", "Results"]
+    selection = [st.sidebar.button(tab, key=tab, help=tab, type ="secondary") for tab in tabs]
 
-        st.sidebar.subheader("Navigation")
 
-        selection = [st.sidebar.button(tab, key=tab, help=tab, type ="secondary") for tab in tabs]
-        return selection
-
-    methodology = e1()
-
-    if methodology[2]:
-        st.sideBar.clear()
-        tabs = ["Home", "Predictive Dashboard", "Methodology", "Results"]
-        methodology_tabs = ["Data Description", "Data Cleaning", "Clustering"]
-        st.write("sidebar")
-
+    return selection
