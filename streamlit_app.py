616d48e54bd54ef8a67244661e58c39c990cf2c9
varun-cloudknox <vkolli@cloudknox.io>
2023-04-28 17:47:07 -0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,10 +13,11 @@ def initializeSideBar():
     if selection[1]:
         interface()
     if all(value is None for value in selection):
-      introduction()
+        introduction()
 
 
 def main():
+    introduction()
     initializeSideBar()
 
 if __name__ == '__main__':
