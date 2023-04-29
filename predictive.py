797737e8d14ceea9a0235f0c1d0e12a35f65edcc
varun-cloudknox <vkolli@cloudknox.io>
2023-04-28 22:57:37 -0700

    update

--- predictive.py
+++ predictive.py
@@ -1,48 +1,113 @@
 import pandas as pd
 import streamlit as st
+# Modeling Packages
+import sklearn
+from sklearn.model_selection import train_test_split   # For Data Partitioning
+from sklearn.linear_model import LinearRegression      # To implement Linear Regression
+from sklearn.feature_selection import RFE              # To implement RFE
+from sklearn.model_selection import StratifiedKFold, KFold              # For creating folds
+from sklearn.model_selection import cross_val_score    # For implementing Cross Validation experiments
+from sklearn.model_selection import GridSearchCV       # To implement GridSearch CV
+from sklearn.model_selection import RandomizedSearchCV # To implement Randomized Search CV
+from sklearn.linear_model import Lasso, Ridge          # To implement Lasso and Ridge Regression
+from sklearn.metrics import classification_report
+#from sklearn.metrics import plot_confusion_matrix
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.tree import DecisionTreeClassifier
 import numpy as np
 
 
+#input = ['2442501', "21-24", "Bipolar" , "12+", "Other Hispanic or Latino origin", "White","Male", "Never married", "Yes", "Part time", "Other", "2", "TN"]
+def convertAge(age):
+    # define subgroups with corresponding age ranges
+    subgroups = {
+        '0-11': range(0, 12),
+        '12-14': range(12, 15),
+        '15-17': range(15, 18),
+        '18-20': range(18, 21),
+        '21-24': range(21, 25),
+        '25-29': range(25, 30),
+        '30-34': range(30, 35),
+        '35-39': range(35, 40),
+        '40-44': range(40, 45),
+        '45-49': range(45, 50),
+        '50-54': range(50, 55),
+        '55-59': range(55, 60),
+        '60-64': range(60, 65),
+        '65 up': range(65, 150)  # use a large upper bound to include all ages 65 and above
+    }
+
+    # classify the age variable
+    for group, age_range in subgroups.items():
+        if age in age_range:
+            age_group = group
+            break
+    else:
+        age_group = '-9'
+
+    # store the age group as a string variable
+    age_group_str = age_group.replace(' ', '')  # remove any spaces
+
+    return age_group_str
+
+
 def prompt():
-
-    subgroups = {'0-11': range(0, 12), '12-14': range(12, 15), '15-17': range(15, 18),
-                 '18-20': range(18, 21), '21-24': range(21, 25), '25-29': range(25, 30),
-                 '30-34': range(30, 35), '35-39': range(35, 40), '40-44': range(40, 45),
-                 '45-49': range(45, 50), '50-54': range(50, 55), '55-59': range(55, 60),
-                 '60-64': range(60, 65), '65 up': range(65, 150)}
-
-    education_levels = ["0 to 8", "9 to 11", "12 or GED", "12+"]
-    employment_statuses = ["Full time", "Part time", "Employed non differentiated", "Unemployed", "Not in labor force"]
-    gender_options = ["Male", "Female"]
-    states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS",
-              "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY",
-              "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
-    housing_situations = ["Homeless", "Private residence", "Other"]
-    ethnicities = ["Mexican", "Puerto Rican", "Other Hispanic or Latino origin", "Not of Hispanic or Latino origin"]
-    marital_status_options = ['Never married', 'Now married', 'Separated', 'Divorced', 'Widowed']
-
-    if 'query' not in st.session_state:
-            st.session_state.query = []
+    if "user" not in st.session_state:
+            st.session_state.user = []
 
     with st.form(key='my_form'):
-       selected_age_group = st.selectbox('Select Age Group', list(subgroups.keys()))
-       educInput = st.selectbox("Select your education level", education_levels)
-       employInput = st.selectbox("Select your employment status", employment_statuses)
-       genderInput = st.radio("Select your gender", options=gender_options)
-       stateInput = st.selectbox("Select a state", states)
-       livArangInput = st.selectbox("Select your living arrangement", housing_situations)
-       ethnicityInput = st.selectbox("Select your Ethnicity", ethnicities)
-       marStatInput = st.selectbox('Select your marital status:', options=marital_status_options)
-       sapInput = st.radio("SAP", options=["Yes", "No"])
+        user = []
+        age = st.number_input(label='Enter Age')
+        ageInput = convertAge(age)
 
-       if st.form_submit_button(label='Predict'):
-           user_input = [selected_age_group, educInput, ethnicityInput, genderInput, marStatInput, sapInput,
-                         employInput, livArangInput, stateInput]
-           st.session_state.query.append(user_input)
-           st.write(st.session_state)
+        education_levels = ["0 to 8", "9 to 11", "12 or GED", "12+"]
+        educInput = st.selectbox("Select your education level", education_levels)
+
+        employment_statuses = ["Full time", "Part time", "Employed non differentiated", "Unemployed", "Not in labor force"]
+        employInput = st.selectbox("Select your employment status", employment_statuses)
+
+        genderInput = st.radio("Select your gender", options=["Male", "Female"])
+
+        stateInput = st.selectbox("Select a state",
+                                                      ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
+                                                       "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+                                                       "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+                                                       "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+                                                       "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"])
 
 
+        housing_situations = ["Homeless", "Private residence", "Other"]
+        livArangInput = st.selectbox("Select your living arrangement", housing_situations)
 
-def predict():
-    prompt()
+        ethnicities = ["Mexican", "Puerto Rican", "Other Hispanic or Latino origin", "Not of Hispanic or Latino origin"]
+
+        ethnicityInput = st.selectbox("Select your Ethnicity", ethnicities)
+
+        marital_status_options = ['Never married', 'Now married', 'Separated', 'Divorced', 'Widowed']
+        marStatInput = st.selectbox('Select your marital status:', options=marital_status_options)
+
+        sapInput = st.radio("SAP", options=["Yes", "No"])
+
+        veteranInput = st.radio("Veteran", options=["Yes", "No"])
+
+        submit_button = st.form_submit_button(label='Run')
+        user_input = []
+        if submit_button:
+            user_input = [ageInput, educInput, ethnicityInput, raceInput, genderInput, marStatInput, sapInput, employInput, livArangInput, veteranInput, stateInput]
+            st.session_state.user.append(user_input)
+
+
+    if st.session_state.user:
+            st.write("User inputs:")
+            display_user_input(user_input)
+
+    st.session_state.user = []
+    return st.session_state.user
+
+def interface():
+    st.title("Backend Interface")
+    seshUser = prompt()
+
+
 
