eb6ef0953645b5e0e8cb5fc8147d1cadea454f70
varun-cloudknox <vkolli@cloudknox.io>
2023-05-02 00:51:34 -0700

    update

--- backendInterface.py
+++ backendInterface.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def process(input):
     df = pd.read_csv('CSV_files/dummieCodex.csv')
-    input = input[0]
+    #input = input[0]
     cols = ['Age Group', 'Education Level', 'Employment Status', 'Sex', 'State', 'Living Arrangement', 'Ethnicity', "Race" 'Marital Status', 'Substance Abuse History', 'Veteran Status', 'Mental Health Diagnosis History']
     st.write(input)
 
