d47bcc62fbd4ebb77c853e3ae98a0f4fc6f03716
varun-cloudknox <vkolli@cloudknox.io>
2023-05-02 10:54:18 -0700

    update

--- backendInterface.py
+++ backendInterface.py
@@ -99,7 +99,7 @@ def process(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, 
 
 
 def prompt():
-
+    st.title("l")
     if "input" not in st.session_state:
         st.session_state.input = None
 
