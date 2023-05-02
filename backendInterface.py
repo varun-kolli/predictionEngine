119feb2dbb8df7ba1d7fd04d29f77a6884e12a64
varun-cloudknox <vkolli@cloudknox.io>
2023-05-01 23:42:50 -0700

    update

--- backendInterface.py
+++ backendInterface.py
@@ -2,68 +2,19 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
-def convertAge(age):
-    # define subgroups with corresponding age ranges
-    subgroups = {
-        '0-11': range(0, 12),
-        '12-14': range(12, 15),
-        '15-17': range(15, 18),
-        '18-20': range(18, 21),
-        '21-24': range(21, 25),
-        '25-29': range(25, 30),
-        '30-34': range(30, 35),
-        '35-39': range(35, 40),
-        '40-44': range(40, 45),
-        '45-49': range(45, 50),
-        '50-54': range(50, 55),
-        '55-59': range(55, 60),
-        '60-64': range(60, 65),
-        '65 up': range(65, 150)  # use a large upper bound to include all ages 65 and above
-    }
+def compute():
 
-    # classify the age variable
-    for group, age_range in subgroups.items():
-        if age in age_range:
-            age_group = group
-            break
-    else:
-        age_group = '-9'
-
-    # store the age group as a string variable
-    age_group_str = age_group.replace(' ', '')  # remove any spaces
-
-    return age_group_str
 
 def smt(input):
     st.write(input)
 
 def prompt():
-    if "user" not in st.session_state:
-        st.session_state.user = []
-
 
     with st.form(key='my_form'):
         user = []
 
-        subgroups = {
-            '0-11': range(0, 12),
-            '12-14': range(12, 15),
-            '15-17': range(15, 18),
-            '18-20': range(18, 21),
-            '21-24': range(21, 25),
-            '25-29': range(25, 30),
-            '30-34': range(30, 35),
-            '35-39': range(35, 40),
-            '40-44': range(40, 45),
-            '45-49': range(45, 50),
-            '50-54': range(50, 55),
-            '55-59': range(55, 60),
-            '60-64': range(60, 65),
-            '65 up': range(65, 150)  # use a large upper bound to include all ages 65 and above
-        }
-
-        ageInput = st.selectbox('Select Age Group', list(subgroups.keys()))
-
+        ageOptions = ['15-17', '18-20', '21-24', '25-29', '30-34', '35-39', '40-44', '45-49', '50-54', '55-59', '60-64', '65 up']
+        ageInput = st.selectbox('Select Age Group', ageOptions)
 
         education_levels = ["0 to 8", "9 to 11", "12 or GED", "12+"]
         educInput = st.selectbox("Select your education level", education_levels)
@@ -104,14 +55,9 @@ def prompt():
 
         if submit:
             form_data = [ageInput, educInput, employInput, genderInput, stateInput, livArangInput, ethnicityInput, raceInput, marStatInput, sapInput, veteranInput, numhs]
+            compute(form_data)
             st.write(form_data)
 
-    if st.session_state.user:
-            st.write("User inputs:")
-            st.write(user_input)
-
-    st.session_state.user = []
-    return st.session_state.user
 
 
 def interface():
