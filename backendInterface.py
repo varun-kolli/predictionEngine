e39ae9ad44e58da6c687b7efdfabfb4fbbf8a9a7
varun-cloudknox <vkolli@cloudknox.io>
2023-05-05 18:50:28 -0700

    update

--- backendInterface.py
+++ backendInterface.py
@@ -82,6 +82,8 @@ def process(query):
 
         displayPrediction(cluster)
         smt = loaded_model.predict_proba(row)
+
+        st.subheader("prediction Probabilities")
         st.table(smt)
 
 
