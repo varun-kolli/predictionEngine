5cbc369c1e962cb202f0a425aa08409d4c58b33b
varun-cloudknox <vkolli@cloudknox.io>
2023-05-07 12:42:26 -0700

    update

--- methodology.py
+++ methodology.py
@@ -4,25 +4,3 @@ import streamlit as st
 def methMain():
     st.title("Methodology")
 
-    st.header("My Page")
-
-    # Create a link to a specific location on the page
-    st.markdown("<a id='section1'></a>", unsafe_allow_html=True)
-
-    # Add a button to navigate to the section
-    if st.button("Go to Section 1"):
-        st.experimental_set_query_params(section="1")
-
-    # Add some content
-    st.write("This is the main content of the page.")
-
-    # Add a section to navigate to
-    st.header("Section 1")
-    st.write("This is the content of Section 1.")
-
-    # Create a link back to the top of the page
-    st.markdown("<a id='top'></a>", unsafe_allow_html=True)
-
-    # Add a button to navigate back to the top
-    if st.button("Back to top"):
-        st.experimental_set_query_params(section=None)
--- sidebar.py
+++ sidebar.py
@@ -6,18 +6,19 @@ def sideBar():
 
     st.sidebar.subheader("Navigation")
     home_button = st.sidebar.button("Home", key="Home", help="Home", type="secondary")
-    dashboard_button = st.sidebar.button("Predictive Dashboard", key="Predictive Dashboard", help="Predictive Dashboard", type="secondary")
     st.sidebar.markdown("<hr>", unsafe_allow_html=True) # add a horizontal partition
 
     st.sidebar.subheader("Methodology")
-    methodology_button = st.sidebar.button("Methodology", key="Methodology", help="Methodology", type="secondary")
     data_description_button = st.sidebar.button("Data Description", key="Data Description", help="Data Description", type="secondary")
     data_cleaning_button = st.sidebar.button("Data Cleaning", key="Data Cleaning", help="Data Cleaning", type="secondary")
     clustering_button = st.sidebar.button("Clustering", key="Clustering", help="Clustering", type="secondary")
     ml_button = st.sidebar.button("Machine Learning", key="Machine Learning", help="Machine Learning", type="secondary")
+    results_button = st.sidebar.button("Results", key="Results", help="Results", type="secondary")
     st.sidebar.markdown("<hr>", unsafe_allow_html=True) # add a horizontal partition
 
-    results_button = st.sidebar.button("Results", key="Results", help="Results", type="secondary")
+    st.sidebar.subheader("Predict Your Diagnosis")
+    dashboard_button = st.sidebar.button("Predictive Dashboard", key="Predictive Dashboard", help="Predictive Dashboard", type="secondary")
+
 
     # Store the buttons in a list
     selection = [home_button, dashboard_button, methodology_button, data_description_button, data_cleaning_button, clustering_button, ml_button, results_button]
