05e259765dba91ad5265d136b682e2a099e4803f
varun-cloudknox <vkolli@cloudknox.io>
2023-05-06 18:09:46 -0700

    update

--- backendInterface.py
+++ backendInterface.py
@@ -163,7 +163,8 @@ def interface():
             data['Question'] = headers
             data['Answer'] = smt
         df = pd.DataFrame(data)
-        st.dataframe(df.set_index('Question'))
+        with st.container():
+            st.dataframe(df.set_index('Question'))
 
     def executeQuery(stuff):
         query = [stuff[0], stuff[1], stuff[6], stuff[7], stuff[3], stuff[8], stuff[9], stuff[2], stuff[5], stuff[10], stuff[4]]
