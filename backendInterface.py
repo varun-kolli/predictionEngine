cf0a768860e9d74b40daa83af9b6e130696686c1
varun-cloudknox <vkolli@cloudknox.io>
2023-05-06 20:07:54 -0700

    update

--- backendInterface.py
+++ backendInterface.py
@@ -44,6 +44,8 @@ def displayPrediction(cluster, query, probs):
     ordered_probs = {str(key): probs_dict[str(key)] for key in sorted_probs}
 
     cluster_values = [ordered_probs[str(i)] for i in range(len(ordered_probs))]
+
+    st.write(cluster_values)
     df['Cluster Value'] = cluster_values
 
     st.write(df)
