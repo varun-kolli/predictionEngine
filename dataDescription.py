b0cfc7b5a22e0f5abbb30fb401278b56fa2e6dde
varun-cloudknox <vkolli@cloudknox.io>
2023-05-07 13:39:37 -0700

    update

--- dataDescription.py
+++ dataDescription.py
@@ -4,34 +4,37 @@ import streamlit as st
 def about():
     st.subheader("About the Data")
 
+def display_dataframe(file_path):
+    df = pd.read_csv(file_path, index_col=0)
+    df = df.rename(columns={df.columns[0]: "", df.columns[1]: "Value"})
+    st.dataframe(df)
+
 def dataCols():
-    st.subheader("Features")
     tab1, tab2, tab4, tab5, tab6, tab7, tab8, tab9, tab10, tab11, tab13, tab14 = st.tabs(["Age", "Disorders", "Education", "Employment","Ethnicity", "Gender", "Living Arrangement", "Marital Status", "Race", "SAP", "States", "Veteran"])
     with tab1:
-        smt = pd.read_csv("CSV_files/age_key.csv")
-        st.dataframe(smt)
+        display_dataframe("CSV_files/age_key.csv")
     with tab2:
-        st.dataframe(pd.read_csv("CSV_files/Disorders_Key.csv"))
+        display_dataframe("CSV_files/Disorders_Key.csv")
     with tab4:
-        st.dataframe(pd.read_csv("CSV_files/educ_key.csv"))
+        display_dataframe("CSV_files/educ_key.csv")
     with tab5:
-        st.dataframe(pd.read_csv("CSV_files/employ_key.csv"))
+        display_dataframe("CSV_files/employ_key.csv")
     with tab6:
-        st.dataframe(pd.read_csv("CSV_files/ethnic_key.csv"))
+        display_dataframe("CSV_files/ethnic_key.csv")
     with tab7:
-        st.dataframe(pd.read_csv("CSV_files/gender_key.csv"))
+        display_dataframe("CSV_files/gender_key.csv")
     with tab8:
-        st.dataframe(pd.read_csv("CSV_files/livarag_key.csv"))
+        display_dataframe("CSV_files/livarag_key.csv")
     with tab9:
-        st.dataframe(pd.read_csv("CSV_files/marstat_key.csv"))
+        display_dataframe("CSV_files/marstat_key.csv")
     with tab10:
-        st.dataframe(pd.read_csv("CSV_files/race_key.csv"))
+        display_dataframe("CSV_files/race_key.csv")
     with tab11:
-        st.dataframe(pd.read_csv("CSV_files/sap_key.csv"))
+        display_dataframe("CSV_files/sap_key.csv")
     with tab13:
-        st.dataframe(pd.read_csv("CSV_files/States_ID.csv"))
+        display_dataframe("CSV_files/States_ID.csv")
     with tab14:
-        st.dataframe(pd.read_csv("CSV_files/veteran_key.csv"))
+        display_dataframe("CSV_files/veteran_key.csv")
 
 def dataDesc():
     st.title("Data Description")
