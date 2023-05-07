5ca0bd301898c804a4857b2354745287ceb9f927
varun-cloudknox <vkolli@cloudknox.io>
2023-05-07 13:45:14 -0700

    update

--- dataDescription.py
+++ dataDescription.py
@@ -26,7 +26,8 @@ def dataCols():
     with tab8:
         display_dataframe("CSV_files/livarag_key.csv")
     with tab9:
-        display_dataframe("CSV_files/marstat_key.csv")
+        #display_dataframe("CSV_files/marstat_key.csv")
+        st.write("hello")
     with tab10:
         display_dataframe("CSV_files/race_key.csv")
     with tab11:
