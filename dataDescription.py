1976ebdd8b2d9c981aa10048a84bd8fe929be34f
varun-cloudknox <vkolli@cloudknox.io>
2023-05-07 13:49:29 -0700

    update

--- dataDescription.py
+++ dataDescription.py
@@ -7,7 +7,7 @@ def about():
 def display_dataframe(file_path):
     df = pd.read_csv(file_path, index_col=0)
     #df = df.rename(columns={df.columns[0]: "", df.columns[1]: "Value"})
-    new_columns = [' ', 'Value']
+    new_columns = ['Value']
     df.columns = new_columns
 
     st.dataframe(df)
