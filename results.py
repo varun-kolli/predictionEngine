91c4a290f3dcf8409f0389d0fb5824738afe4939
varun-cloudknox <vkolli@cloudknox.io>
2023-05-07 23:26:31 -0700

    update

--- results.py
+++ results.py
@@ -4,7 +4,7 @@ import streamlit as st
 def resultMain():
     st.title("Results")
 
-    cols = st.columns(3)
+    cols = st.columns(2)
 
     with cols[0]:
         st.subheader("Classification Report")
@@ -27,22 +27,25 @@ def resultMain():
         img = open(img_path, "rb").read()
         st.image(img, use_column_width=True)
 
-    with cols[2]:
-        st.subheader("Feature Importance")
-        data = {
-            "Feature": ["STATEFIP", "AGE", "MARSTAT", "GENDER", "EDUC", "EMPLOY", "NUMMHS", "SAP", "LIVARAG", "RACE", "ETHNIC"],
-            "Importance": [0.247583, 0.239926, 0.14362, 0.090526, 0.079785, 0.07844, 0.037469, 0.033961, 0.021968, 0.021573, 0.00515]
-        }
 
-        # create a Pandas dataframe from the dictionary
-        df = pd.DataFrame(data).set_index("Feature")
+    st.subheader("Feature Importance")
+    data = {
+        "Feature": ["STATEFIP", "AGE", "MARSTAT", "GENDER", "EDUC", "EMPLOY", "NUMMHS", "SAP", "LIVARAG", "RACE", "ETHNIC"],
+        "Importance": [0.247583, 0.239926, 0.14362, 0.090526, 0.079785, 0.07844, 0.037469, 0.033961, 0.021968, 0.021573, 0.00515]
+    }
 
-        # create a Streamlit table from the dataframe
-        st.dataframe(df)
+    # create a Pandas dataframe from the dictionary
+    df = pd.DataFrame(data).set_index("Feature")
 
-        st.write(" ")
+    # transpose the dataframe
+    df = df.T
 
-        st.subheader("Decision Tree")
-        img_path = "images/modes_decision_tree-1.png"
-        img = open(img_path, "rb").read()
-        st.image(img, use_column_width=True)
+    # create a Streamlit table from the transposed dataframe
+    st.table(df)
+
+    st.write(" ")
+
+    st.subheader("Decision Tree")
+    img_path = "images/modes_decision_tree-1.png"
+    img = open(img_path, "rb").read()
+    st.image(img, use_column_width=True)
