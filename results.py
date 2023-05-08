4f27ec8841eee228c444610ff0e4c374eccf5c06
varun-cloudknox <vkolli@cloudknox.io>
2023-05-07 23:23:08 -0700

    update

--- results.py
+++ results.py
@@ -4,40 +4,45 @@ import streamlit as st
 def resultMain():
     st.title("Results")
 
-    st.subheader("Classification Report")
+    cols = st.columns(3)
 
-    data = {
-        'precision': [0.63, 0.62, 0.54],
-        'recall': [0.66, 0.61, 0.02],
-        'f1-score': [0.64, 0.62, 0.04],
-        'support': [204486, 194977, 6017]
-    }
+    with cols[0]:
+        st.subheader("Classification Report")
 
-    df = pd.DataFrame(data)
+        data = {
+            'precision': [0.63, 0.62, 0.54],
+            'recall': [0.66, 0.61, 0.02],
+            'f1-score': [0.64, 0.62, 0.04],
+            'support': [204486, 194977, 6017]
+        }
 
-    st.dataframe(df)
+        df = pd.DataFrame(data)
 
-    st.subheader("Confusion Matrix")
+        st.dataframe(df)
 
-    img_path = "images/cf_mat.png"
-    img = open(img_path, "rb").read()
-    st.image(img, use_column_width=True)
+    with cols[1]:
+        st.subheader("Confusion Matrix")
 
-    st.subheader("Feature Importance")
-    data = {
-        "Feature": ["STATEFIP", "AGE", "MARSTAT", "GENDER", "EDUC", "EMPLOY", "NUMMHS", "SAP", "LIVARAG", "RACE", "ETHNIC"],
-        "Importance": [0.247583, 0.239926, 0.14362, 0.090526, 0.079785, 0.07844, 0.037469, 0.033961, 0.021968, 0.021573, 0.00515]
-    }
+        img_path = "images/cf_mat.png"
+        img = open(img_path, "rb").read()
+        st.image(img, use_column_width=True)
 
-    # create a Pandas dataframe from the dictionary
-    df = pd.DataFrame(data).set_index("Feature")
+    with cols[2]:
+        st.subheader("Feature Importance")
+        data = {
+            "Feature": ["STATEFIP", "AGE", "MARSTAT", "GENDER", "EDUC", "EMPLOY", "NUMMHS", "SAP", "LIVARAG", "RACE", "ETHNIC"],
+            "Importance": [0.247583, 0.239926, 0.14362, 0.090526, 0.079785, 0.07844, 0.037469, 0.033961, 0.021968, 0.021573, 0.00515]
+        }
 
-    # create a Streamlit table from the dataframe
-    st.dataframe(df)
+        # create a Pandas dataframe from the dictionary
+        df = pd.DataFrame(data).set_index("Feature")
 
-    st.write(" ")
+        # create a Streamlit table from the dataframe
+        st.dataframe(df)
 
-    st.subheader("Decision Tree")
-    img_path = "images/modes_decision_tree-1.png"
-    img = open(img_path, "rb").read()
-    st.image(img, use_column_width=True)
+        st.write(" ")
+
+        st.subheader("Decision Tree")
+        img_path = "images/modes_decision_tree-1.png"
+        img = open(img_path, "rb").read()
+        st.image(img, use_column_width=True)
