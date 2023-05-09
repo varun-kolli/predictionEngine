059d92aad5461ba263bc9baf6a3dfb70d45822bb
varun-cloudknox <vkolli@cloudknox.io>
2023-05-08 17:34:34 -0700

    update

--- backendInterface.py
+++ backendInterface.py
@@ -151,7 +151,6 @@ def displayInput(stuff):
 
 def interface():
     st.title("Backend Interface")
-    st.button("hi")
     if 'stage' not in st.session_state:
         st.session_state.stage = 0
         st.session_state.input = []
@@ -161,7 +160,10 @@ def interface():
         st.session_state.stage = stage
         st.session_state.input= input
 
-        interface()
+        stuff = input
+        query = [stuff[0], stuff[1], stuff[6], stuff[7], stuff[3], stuff[8], stuff[9], stuff[2], stuff[5], stuff[10], stuff[4]]
+
+        process(query)
         #displayInput(input)
 
     # Some code
@@ -208,17 +210,6 @@ def interface():
 
     #if st.session_state.stage > 0 and st.session_state.input != ['15-17', '0 to 8', 'Full time', 'Male', 'AL', 'Homeless', 'Mexican', 'Native', 'Never married', 'Yes', 1]:
 
-    if st.session_state.stage > 0:
-        stuff = st.session_state.input
-        smt = [stuff[0], stuff[1], stuff[6], stuff[7], stuff[3], stuff[8], stuff[9], stuff[2], stuff[5], stuff[10], stuff[4]]
-        headers = ['AGE', 'EDUC', 'ETHNIC', 'RACE', 'GENDER', 'MARSTAT', 'SAP', 'EMPLOY', 'LIVARAG', 'NUMMHS', 'STATEFIP']
-        data = {}
-        for i in range(len(headers)):
-            data['Question'] = headers
-            data['Answer'] = smt
-            df = pd.DataFrame(data)
-        with st.container():
-            st.table(df.set_index('Question').T)
 
     def executeQuery(stuff):
         query = [stuff[0], stuff[1], stuff[6], stuff[7], stuff[3], stuff[8], stuff[9], stuff[2], stuff[5], stuff[10], stuff[4]]
@@ -226,5 +217,4 @@ def interface():
         process(query)
 
 
-    if st.session_state.stage > 0:
-        st.button('View Prediction', on_click=executeQuery, args=(st.session_state.input, ))
+
