3cce66a4f2ee6ea45cd6e69a03880001f431bfdd
varun-cloudknox <vkolli@cloudknox.io>
2023-05-08 17:18:58 -0700

    update

--- backendInterface.py
+++ backendInterface.py
@@ -206,11 +206,7 @@ def interface():
                     [agestuff, educstuff, employstuff, genderstuff, statestuff, livArangstuff, ethnicitystuff, racestuff, marStatstuff, sapstuff, numhs]))
 
     #if st.session_state.stage > 0 and st.session_state.input != ['15-17', '0 to 8', 'Full time', 'Male', 'AL', 'Homeless', 'Mexican', 'Native', 'Never married', 'Yes', 1]:
-    if st.session_state.stage == 2:
-        stuff = st.session_state.input
-    else:
-        stuff = [""]*11
-
+    stuff = st.session_state.input
     smt = [stuff[0], stuff[1], stuff[6], stuff[7], stuff[3], stuff[8], stuff[9], stuff[2], stuff[5], stuff[10], stuff[4]]
     headers = ['AGE', 'EDUC', 'ETHNIC', 'RACE', 'GENDER', 'MARSTAT', 'SAP', 'EMPLOY', 'LIVARAG', 'NUMMHS', 'STATEFIP']
     data = {}
