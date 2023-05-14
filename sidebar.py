d5ec6254eda4697211ba32f1ccb26ed514155ca0
varun-cloudknox <vkolli@cloudknox.io>
2023-05-14 14:45:51 -0700

    update

--- sidebar.py
+++ sidebar.py
@@ -24,20 +24,21 @@ def sideBar():
     tabs = ["Home", "Predictive Dashboard", "Methodology", "Data Description", "Data Cleaning", "Clustering", "Machine Learning", "Results"]
 
     st.sidebar.subheader("Navigation")
-    home_button = st.sidebar.button("Home", key="Home", type="primary")
-    dashboard_button = st.sidebar.button("Predictive Dashboard", key="Predictive Dashboard", type="primary")
+    home_button = st.sidebar.button("Home", key="Home", type="secondary")
+    dashboard_button = st.sidebar.button("Predictive Dashboard", key="Predictive Dashboard", type="secondary")
     st.write(" ")
     st.sidebar.subheader("Methodology")
-    data_description_button = st.sidebar.button("Data Description", key="Data Description", type="primary")
-    data_cleaning_button = st.sidebar.button("Data Cleaning", key="Data Cleaning", type="primary")
-    clustering_button = st.sidebar.button("Clustering", key="Clustering", type="primary")
-    ml_button = st.sidebar.button("Machine Learning", key="Machine Learning", type="primary")
-    results_button = st.sidebar.button("Results", key="Results", type="primary")
+    data_description_button = st.sidebar.button("Data Description", key="Data Description", type="secondary")
+    data_cleaning_button = st.sidebar.button("Data Cleaning", key="Data Cleaning", type="secondary")
+    clustering_button = st.sidebar.button("Clustering", key="Clustering", type="secondary")
+    ml_button = st.sidebar.button("Machine Learning", key="Machine Learning", type="secondary")
+    results_button = st.sidebar.button("Results", key="Results", type="secondary")
 
     # Store the buttons in a list
     selection = [home_button, dashboard_button, data_description_button, data_cleaning_button, clustering_button, ml_button, results_button]
 
     # Return the list of buttons
     return selection
+
 
 
