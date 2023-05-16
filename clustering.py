cfccc0ee039f47d1af3901a10443bc174dab561f
varun-cloudknox <vkolli@cloudknox.io>
2023-05-16 16:47:47 -0700

    update

--- clustering.py
+++ clustering.py
@@ -21,43 +21,52 @@ def clustMain():
 
     st.caption(" 'Cluster #' % represents proportion of total cases")
     st.caption(" 'Disorder' % represents proportion of cases within cluster")
+
     col1, col2, col3 = st.columns(3)
-    st.write(" ")
-    st.write(" ")
 
-    st.write(" ")
+    # Data for clusters
+    cluster_0_dict = {
+        "Trauma-related": 0.3508,
+        "Anxiety": 0.2792,
+        "ADHD": 0.1751,
+        "Substance abuse ": 0.0741,
+        "Oppositional defiant": 0.0431,
+        "Pervasive developmental": 0.023,
+        "Personality": 0.0189,
+        "Conduct ": 0.034,
+        "Delirium/dementia": 0.0067
+    }
 
+    cluster_1_dict = {
+        "Depression": 0.712,
+        "Bipolar": 0.2881
+    }
 
-    # Display the disorders in cluster 0 in the first column
+    # Convert the data to pandas DataFrame and normalize it
+    df_cluster_0 = pd.DataFrame(list(cluster_0_dict.items()), columns=['Disorder', 'Percentage'])
+    df_cluster_1 = pd.DataFrame(list(cluster_1_dict.items()), columns=['Disorder', 'Percentage'])
+
+    # Create an Altair bar chart for each cluster
+    chart_cluster_0 = alt.Chart(df_cluster_0).mark_bar().encode(
+        x='Percentage',
+        y='Disorder'
+    )
+
+    chart_cluster_1 = alt.Chart(df_cluster_1).mark_bar().encode(
+        x='Percentage',
+        y='Disorder'
+    )
+
+    # Display the charts in Streamlit
     with col1:
         st.write("**Cluster 0: 46.8%**")
-        # Cluster 0 dictionary
-        cluster_0_dict = {
-                "Trauma-related": 0.3508,
-                "Anxiety": 0.2792,
-                "ADHD": 0.1751,
-                "Substance abuse ": 0.0741,
-                "Oppositional defiant": 0.0431,
-                "Pervasive developmental": 0.023,
-                "Personality": 0.0189,
-                "Conduct ": 0.034,
-                "Delirium/dementia": 0.0067
-        }
-        for disorder, percentage in cluster_0_dict.items():
-            st.write(f"- {disorder}: {percentage:.2%}")
+        st.altair_chart(chart_cluster_0, use_container_width=True)
 
-
-    # Display the disorders in cluster 1 in the second column
     with col2:
         st.write("**Cluster 1: 36.3%**")
-        cluster_1_dict = {
-            "Depression": 0.712,
-            "Bipolar": 0.2881
-        }
-        for disorder, percentage in cluster_1_dict.items():
-                st.write(f"- {disorder}: {percentage:.2%}")
+        st.altair_chart(chart_cluster_1, use_container_width=True)
 
-    # Display the disorders in cluster 2 in the third column
     with col3:
         st.write("**Cluster 2: 11.7%**")
-        st.write("- " + "\n- ".join(clusters[2]))
+        st.write("**Schizophrenia/psychotic** is the only disorder in this cluster")
+
